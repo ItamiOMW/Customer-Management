b29930a0eae4c94e1912c12962b32b86c1cf960d
Вадим <itamiomw@gmail.com>
2023-04-07 13:30:41 +0300

    Inheritance and templates

--- CustomerManagement/wsgi.py
+++ CustomerManagement/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CustomerManagment project.
+WSGI config for CustomerManagement project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CustomerManagment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CustomerManagement.settings')
 
 application = get_wsgi_application()
--- accounts/views.py
+++ accounts/views.py
@@ -5,10 +5,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse("Product Page")
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse("Customer Page")
+    return render(request, 'accounts/customer.html')
