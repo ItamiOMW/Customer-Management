94d7310aee0ce1aeee420f960796eb42c740dfe3
Вадим <itamiomw@gmail.com>
2023-04-07 12:10:00 +0300

    Urls and views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
 # Create your views here.
+def home(request):
+    return HttpResponse("Home Page")
+
+def products(request):
+    return HttpResponse("Product Page")
+
+def customer(request):
+    return HttpResponse("Customer Page")
