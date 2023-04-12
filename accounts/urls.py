239dfcea94a35cdfef1f33ed23f146f46bf8a1f3
Вадим <itamiomw@gmail.com>
2023-04-12 10:24:21 +0300

    Inline formset

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('products/', views.products, name="products"),
     path('customer/<str:primary_key>/', views.customer, name="customer"),
 
-    path('create_order/', views.create_order, name="create_order"),
+    path('create_order/<str:primary_key>/', views.create_order, name="create_order"),
     path('update_order/<str:primary_key>/', views.update_order, name="update_order"),
     path('delete_order/<str:primary_key>/', views.delete_order, name="delete_order")
 ]
