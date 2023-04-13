c5f7f0703c570f5b7aec5eb99b7a139e1fcf9ed8
Вадим <itamiomw@gmail.com>
2023-04-13 09:45:43 +0300

    Authentication, Registration, Permissions

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerUser, name='register'),
+    path('login/', views.loginUser, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
     path('', views.home, name="home"),
+    path('user/', views.user, name='user'),
     path('products/', views.products, name="products"),
     path('customer/<str:primary_key>/', views.customer, name="customer"),
 
