a0f3928ff47dcc93b90ca0daeba8304341225b5a
Вадим <itamiomw@gmail.com>
2023-04-14 10:21:39 +0300

    Reset password

--- CustomerManagement/settings.py
+++ CustomerManagement/settings.py
@@ -131,3 +131,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'itamiomw@gmail.com'
+EMAIL_HOST_PASSWORD = 'some fake password'
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,34 @@
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     path('register/', views.registerUser, name='register'),
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+
+    path(
+        'reset_password/',
+        auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+        name='reset_password'
+    ),
+    path(
+        'reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_done.html'),
+        name='password_reset_confirm'
+    ),
 
     path('', views.home, name="home"),
     path('user/', views.user, name='user'),
@@ -16,5 +39,5 @@ urlpatterns = [
 
     path('create_order/<str:primary_key>/', views.create_order, name="create_order"),
     path('update_order/<str:primary_key>/', views.update_order, name="update_order"),
-    path('delete_order/<str:primary_key>/', views.delete_order, name="delete_order")
+    path('delete_order/<str:primary_key>/', views.delete_order, name="delete_order"),
 ]
